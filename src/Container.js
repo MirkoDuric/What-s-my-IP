417a101ae80187d3c4f8c861312d5761decb535c
MirkoDuric <mirkodjuricczv@gmail.com>
2023-02-18 12:22:41 +0100

    Marker deleted

--- src/Container.js
+++ src/Container.js
@@ -35,7 +35,6 @@ export default function Container() {
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               ></TileLayer>
-              <Marker position={[lat, lng]}></Marker>
             </MapContainer>
           </div>
           <div className="dataContainer">
