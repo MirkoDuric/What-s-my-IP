cf4b0899ef3ed85d363a82aef55efb774e6e61bc
MirkoDuric <mirkodjuricczv@gmail.com>
2023-06-04 23:38:34 +0200

    Error handling added.

--- src/Container.js
+++ src/Container.js
@@ -1,15 +1,12 @@
 import { MapContainer } from "react-leaflet/MapContainer";
 import { TileLayer } from "react-leaflet/TileLayer";
-import { Marker } from "react-leaflet/Marker";
-import { Popup } from "react-leaflet/Popup";
 import "leaflet/dist/leaflet.css";
 import useGetApi from "./useGetApi";
 import { useState, useEffect } from "react";
-import { DateTime } from "luxon";
 
 export default function Container() {
   const { DateTime } = require("luxon");
-  let { data, lat, lng } = useGetApi(
+  const { data, lat, lng, error, isLoading } = useGetApi(
     `https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.REACT_APP_API_KEY}`
   );
   const [flag, setFlag] = useState([]);
@@ -25,6 +22,14 @@ export default function Container() {
     }
   }, [data]);
 
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
     <div className="container">
       {Object.keys(data).length ? (
@@ -39,12 +44,12 @@ export default function Container() {
           </div>
           <div className="dataContainer">
             {flag.length ? (
-              <img className="flag" src={flag[0].flags.svg}></img>
+              <img className="flag" src={flag[0].flags.svg} alt="Flag"></img>
             ) : null}
             <div className="text-container">
               <h4>Your IP address is: {data.ip}</h4>
               <p>
-                You are located in: {data.location.city},{" "}
+                Server is located in: {data.location.city},{" "}
                 {data.location.country}
               </p>
               <p>
--- src/useGetApi.js
+++ src/useGetApi.js
@@ -5,22 +5,29 @@ export default function useGetApi(url) {
   const [data, setData] = useState([]);
   const [lat, setLat] = useState(null);
   const [lng, setLng] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    axios
-      .get(url)
-      .then((response) => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url);
         console.log(response.data);
         setData(response.data);
         if (response.data.location) {
           setLat(response.data.location.lat);
           setLng(response.data.location.lng);
         }
-        console.log(lat, lng);
-      })
-      .catch((error) => {
+        setIsLoading(false);
+      } catch (error) {
         console.error(error);
-      });
-  }, []);
-  return { data, lat, lng };
+        setError(error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, lat, lng, error, isLoading };
 }
